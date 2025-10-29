e31eadede6a14d136928ff191b0947e99bfe1267
Thom Breugelmans <thombreugelmans@outlook.com>
2025-10-29 22:43:13 +0100

    chore: sim with nat immunity and plot strategy comparisons

--- plotting_from_cache.py
+++ plotting_from_cache.py
@@ -10,38 +10,46 @@ class VaccinationStrategy(Enum):
     BETWEENNESS = 4
     BETWEENNESS_TIME = 5
     WTS = 6
+    RANDOM = 7
+    NONE = 8
 
 CACHE_DIR = './cache'
 OPTIONS = {
     "max_steps":            1000,
     "start":                1000,
-    "vaccine_strategy":     VaccinationStrategy.DEGREE,
+    "vaccine_strategy":     VaccinationStrategy.RANDOM,
     "vaccine_fraction":     0.1,
-    "immunity_decay_rate":  0.990,
-    "use_natural_immunity": False       # the natural immunity case, sets immunity to 1 if infected
+    "immunity_decay_rate":  0.998,
+    "use_natural_immunity": True       # the natural immunity case, sets immunity to 1 if infected
 }
-EXPERIMENT_NAME: str = "sis_sim_" + ','.join(f'{k}={v}' for k,v in OPTIONS.items())
 
-cache = f'{CACHE_DIR}/{EXPERIMENT_NAME}'
+plt.figure(figsize=(8,5))
+for strat in VaccinationStrategy:
+    OPTIONS["vaccine_strategy"] = strat
+    EXPERIMENT_NAME: str = "sis_sim_" + ','.join(f'{k}={v}' for k,v in OPTIONS.items())
+    cache = f'{CACHE_DIR}/{EXPERIMENT_NAME}'
+    tiots = []
+    for i in range(10):
+        graph_fn = f'{cache}/{i}.gt'
+        sim_fn = f'{cache}/{i}.npy'
+        tiot_fn = f'{cache}/tiot_{i}.npy'
 
-for i in range(10):
-    graph_fn = f'{cache}/{i}.gt'
-    sim_fn = f'{cache}/{i}.npy'
-    #tiot_fn = f'{cache}/tiot_{i}.npy'
+        #graph = gt.load_graph(graph_fn)
+        #sim = np.load(sim_fn, allow_pickle=False)
+        tiot = np.load(tiot_fn, allow_pickle=False)
+        
+        tiots.append(tiot)
 
-    graph = gt.load_graph(graph_fn)
-    sim = np.load(sim_fn, allow_pickle=False)
-    #tiot = np.load(tiot_fn, allow_pickle=False)
+    # compute average tiot
+    average_tiot = [sum(x)/len(x) for x in zip(*tiots)]
 
     # plotting the cumulative infected
-    cumulative_infected = graph.vertex_properties["cumulative_infected"]
-    data = [0]
-    for j,ci in enumerate(cumulative_infected):
-        data.append(data[j-1]+ci)
-    plt.plot(data, label=f'run={i+1}')
+    plt.plot(average_tiot, label=f'strategy={str(strat).removeprefix("VaccinationStrategy.")}')
     
 plt.legend()
 plt.xlabel("Time")
 plt.ylabel("Cumulative infected")
-plt.title("Cumulative Infected over Time")
-plt.savefig(f'plots/cumulative_infected_{EXPERIMENT_NAME}.png')
+plt.title(f"Cumulative Infected over Time per Vaccination Strategy With{'' if OPTIONS["use_natural_immunity"] else 'out'} Natural Immunity")
+plt.tight_layout()
+plt.grid(True, linestyle="--", alpha=0.6)
+plt.savefig(f'plots/cumulative_infected_all_strategies_combined{'_with_nat_immunity' if OPTIONS["use_natural_immunity"] else ''}.png')
